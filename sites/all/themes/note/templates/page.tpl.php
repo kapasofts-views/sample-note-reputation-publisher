7ca7989a48114dae318467f0d468ba08802d21cb
Margots Kapacs <makapacs@hotmail.com>
2013-08-07 20:37:07 -0500

    Hotfix-0.0.1.1
    content updated and added photo

--- sites/all/themes/note/templates/page.tpl.php
+++ sites/all/themes/note/templates/page.tpl.php
@@ -3,9 +3,9 @@
     <div class="full-width">
         <hgroup>
             <h1>Aleksandra Dementjeva Parādā</h1>
-            <h2><i>Informējam:</i> Aleksandra Dementjeva nav nokārtojusi financiālās saistības un NAV uzticības cienīga</h2>
+            <h2><i>Informējam:</i> Aleksandra Dementjeva nav nokārtojusi finansiālās saistības un NAV uzticības cienīga</h2>
         </hgroup>
-        <?php print '<a href="'.base_path() . path_to_theme() .'/img/Aleksandra_Dementjeva_Portrets.jpeg" data-rel="prettyPhoto" title="Aleksandra Dementjeva Parādā"><img class="border center" src="'.base_path() . path_to_theme() .'/img/Aleksandra_Dementjeva_Portrets.jpeg" width="93" height="105" alt="Aleksandra Dementjeva Nav nokārtojusi Finansiālās saistības" /></a>';?>
+        <?php print '<a href="'.base_path() . path_to_theme() .'/img/Aleksandra_Dementjeva_Portrets.jpg" data-rel="prettyPhoto" title="Aleksandra Dementjeva Parādā"><img class="border center" src="'.base_path() . path_to_theme() .'/img/Aleksandra_Dementjeva_Portrets.jpg" width="93" height="105" alt="Aleksandra Dementjeva Nav nokārtojusi Finansiālās saistības" /></a>';?>
         <p><a role="button" class="b-nonemt btn btn-inverse" href="#zinojums" data-toggle="modal">Kā Noņemt Šo Publikāciju?</a>
         </p>
     </div>
@@ -16,14 +16,12 @@
             <div class="col1">
                 <h2>Par Aleksandru Dementjevu</h2>
                 <dl class="dl-horizontal">
-                    <dt>Dzimusi</dt><dd>8 Augusts 1985 gadā</dd>
+                    <dt>Dzimusi</dt><dd>8. Novembrī, 1985</dd>
                     <dt>Dzīves Vieta</dt><dd>Rīga, Latvija</dd>
-                    <dt>Darba Vieta</dt><dd>?</dd>
                 </dl>
                 <h2>Notikumi</h2>
                 <dl>
-                    <dt>Aprlī, 2010 g.</dt><dd>Aleksandra Dementjeva izīrē dzvkl 25 Stabu 46/48, Riga</dd>
-                    <dt>1 Maijā, 2010 g.</dt><dd>Aleksandra Dementjeva ievācās dzvkl 25 Stabu 46/48, Riga</dd>
+                    <dt>1 Maijā, 2010 g.</dt><dd>Aleksandra Dementjeva paraksta īres līgumu(nr 01.05.2010/01) un ievācās 2 ist dzīvoklī Rīgā, Stabu ielā 46/48, dzkl 25</dd>
                     <dt>30 Jūnijs, 2013 g.</dt><dd>Aleksandra Dementjeva izvācās no dzvkl 25 Stabu 46/48, Riga</dd>
                 </dl>
 
@@ -31,8 +29,8 @@
             <div class="col2">
                 <h2>Nenokārtotās Saistības</h2>
                 <ol>
-                    <li>Aleksandra Dementjeva <u>nav nomaksājusi</u> savu elektrības rēķinu par 1,119 lvl</li>
-                    <li>Aleksandra Dementjeva <u>nav nomaksājusi</u> īres maksājumu par 226.16 eur </li>
+                    <li>Aleksandra Dementjeva <u>nav nomaksājusi</u> par izlietoto elektroenerģiju par periodu no 2012. Maija - 2013. Jūnijam. Kopā par LVL1,119</li>
+                    <li>Aleksandra Dementjeva <u>nav nomaksājusi</u> parādu par īri kopā EUR226.16</li>
                 </ol>
 
                 <br />
@@ -47,7 +45,7 @@
     <div id="work-pages">
         <div class="tab">
             <div class="full-width">
-              <p>Visi pierādījumi par šīm Aleksandra Dementjeva nenokārtotām financiālajām saistībām ir pieejami pēc pieprasījuma. Lūdzu sazinaties ar publicētāju aizpildot kontakt formu</p>
+              <p>Visi pierādījumi par Aleksandras Dementjevas nenokārtotajām finansiālajām saistībām ir pieejami pēc pieprasījuma. </p>
                 <a role="button" class="b-nonemt btn btn-inverse" href="#zinojums" data-toggle="modal">Kā Noņemt Šo Publikāciju?</a>
             </div>
         </div>
